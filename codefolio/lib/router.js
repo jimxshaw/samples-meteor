ff21b658d9160a6f12efb56b4cd1852f4d5fff84
JXS <jimxshaw@gmail.com>
2015-09-13 11:39:51 -0400

    added routes

--- codefolio/lib/router.js
+++ codefolio/lib/router.js
@@ -7,4 +7,23 @@ Router.map(function() {
     path: '/',
     template: 'home'
   });
+
+  this.route('about', {
+    path: '/about',
+    template: 'about'
+  });
+  this.route('work', {
+    path: '/work',
+    template: 'work'
+  });
+
+  this.route('blog', {
+    path: '/blog',
+    template: 'blog'
+  });
+
+  this.route('contact', {
+    path: '/contact',
+    template: 'contact'
+  });
 });
